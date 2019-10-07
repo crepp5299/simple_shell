4d32270ce9831500a8dc9e3f5dede9dfff3f510d
Crepp <tan.thai.2t@gmail.com>
2019-10-07 23:23:19 +0700

    update dup2

--- hdh_new/main.c
+++ hdh_new/main.c
@@ -21,24 +21,36 @@ int main()
         char* getLast;
         readInput(cmd, args, &isRunning, &getLast, &run, &fileName);
         if(run != 0) {
-            int f;
-            if (run == 2) {
-                f = open(fileName, O_WRONLY | O_CREAT);
-                dup2(f, STDOUT_FILENO);
-            }
+
             if (strcmp(args[0], "exit") == 0)
                 break;
             pid_t pid;
-            int status;
+
             if ((pid = fork()) < 0) {
                 printf("*ERROR:forking child process failed\n");
             } else if (pid == 0) {
-                int exe = execvp(args[0], args);
+                int f;
+                int exe;
+                if(run == 2) {
+                    f = open(fileName, O_WRONLY | O_CREAT);
+                    dup2(f, STDOUT_FILENO);
+                    exe = execvp(args[0], args);
+                    close(STDOUT_FILENO);
+                    close(f);
+                }
+                else if(run == 3){
+                    f = open(fileName, O_RDONLY | O_CREAT);
+                    dup2(f, STDIN_FILENO);
+                    exe = execvp(args[0], args);
+                    close(STDIN_FILENO);
+                    close(f);
+                }
+                else{
+                    exe = execvp(args[0], args);
+                }
                 if (exe < 0) {
                     printf("*ERROR:exec failed\n");
                 }
-                close(STDOUT_FILENO);
-                close(f);
             } else {
                 if (strcmp(getLast, "&") != 0) {
                     wait(NULL);
